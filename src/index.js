6af5bc9f3c128c8d636a5ae4f0b34ef0fde418e7
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-24 01:07:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="bc_react_58_Lifuss">
+      <BrowserRouter basename="/bc_react_58_Lifuss">
         <PersistGate loading={null} persistor={persistor}>
           <App />
           <ToastContainer
